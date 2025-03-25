74aa5283e0ca1cfb7bd7dac607d1234abece3c4a
Eli Kinkade <42971395+ekinkade21@users.noreply.github.com>
2025-03-25 18:04:15 -0400

    change site and base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,11 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  site: 'https://ekinkade21.github.io',
+  site: 'https://ekinkade21.github.io/docs',
   integrations: [tailwind(), react()],
   output: 'static', // Ensures Astro generates a static site
   build: {
-    outDir: 'docs', // Astro will generate the static site in `docs/`
+    outDir: 'dist', // Astro will generate the static site in `docs/`
   },
-  base: '/dist' 
+  base: '/' 
 });
