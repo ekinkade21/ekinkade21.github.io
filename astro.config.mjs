167f56633b2417c45e8bc3450d37ab3af16a93ff
Eli Kinkade <42971395+ekinkade21@users.noreply.github.com>
2025-03-25 17:21:42 -0400

    change base to docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ export default defineConfig({
   site: 'https://ekinkade21.github.io',
   integrations: [tailwind(), react()],
   output: 'static', // Ensures Astro generates a static site
-  base: '/', // Set this to your repo name if using a project site (e.g., "/my-portfolio/")
   build: {
-    outDir: 'docs', // Astro will generate the static site in `dist/`
-  }
+    outDir: 'dist', // Astro will generate the static site in `dist/`
+  },
+  base: '/docs' 
 });
