42d0d1a4f8321fed948d1b4fb10689cb4f0d072d
Eli Kinkade <42971395+ekinkade21@users.noreply.github.com>
2025-03-25 16:37:47 -0400

    astro!

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,9 @@ import react from '@astrojs/react';
 export default defineConfig({
   site: 'https://ekinkade21.github.io',
   integrations: [tailwind(), react()],
+  output: 'static', // Ensures Astro generates a static site
+  base: '/', // Set this to your repo name if using a project site (e.g., "/my-portfolio/")
+  build: {
+    outDir: 'docs', // Astro will generate the static site in `dist/`
+  }
 });
